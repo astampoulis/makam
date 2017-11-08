afcabcacd42e6b726bd70d7a252904f5a9bc55ea
Antonis Stampoulis <antonis.stampoulis@originate.com>
2017-11-08 16:15:23 -0500

    fix multiline input on node repl

--- js/index.js
+++ js/index.js
@@ -3,7 +3,15 @@ const makam = require("./makam");
 function repl() {
   console.log(`\n\tMakam, version ${makam.version} (Javascript REPL)\n\n`);
   const nodeRepl = require("repl");
-  nodeRepl.start({ eval: function (cmd, context, filename, cb) { makam.eval(cmd); cb(null); } });
+  nodeRepl.start({
+    eval: function (cmd, context, filename, cb) {
+      var cmdTrimmed = cmd.trim();
+      if (![".", "?"].includes(cmdTrimmed[cmdTrimmed.length - 1])) {
+        return cb(new nodeRepl.Recoverable());
+      } else {
+        makam.eval(cmd); cb(null);
+      }
+    }});
 }
 
 if (require.main === module) repl();
