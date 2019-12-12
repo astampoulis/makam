49fb6dc33a4bb53096140fe8e60ccff2016168c1
Antonis Stampoulis <antonis.stampoulis@originate.com>
2019-12-12 14:38:59 -0500

    [circle-skip] attempt (2)

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -25,7 +25,7 @@ fi
 
 # Install OPAM and OCaml
 
-rvm install ruby-2.6
+rvm install 2.6
 rvm use 2.6
 
 brew install gpatch https://raw.githubusercontent.com/Homebrew/homebrew-core/7cfc8ee1d8d2bff2f7545170936a9abdb4662547/Formula/opam.rb || true
